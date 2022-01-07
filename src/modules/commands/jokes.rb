fe024bb8053434b251cfd2be3055571e4fb2a9e3
Hantzu <pvnobres@gmail.com>
2022-01-06 21:54:45 -0300

    0.0.0.17

--- src/modules/commands/jokes.rb
+++ src/modules/commands/jokes.rb
@@ -4,39 +4,32 @@ module Bot::DiscordCommands
       command :Wyxill do |msg|
         msg.respond "```DETERMINATION...
 but nobody came....```"
-      end
     end
 
   command :Dionmid do |msg|
     msg.respond "```“per aspera ad astra…
 …ok, sapete perché i pesci hanno le spine? Perché in acqua c’è la corrente!”```"
     msg.respond "https://media.discordapp.net/attachments/919517210140114987/928811307786989568/IMG_0131.jpg"
-  end
 end
 
 command :Lian do |msg|
     msg.respond "```“Follow me for more advices”```"
     msg.respond "https://media.discordapp.net/attachments/919517210140114987/928811005180522556/Buonanno-con-spigola.jpg"
-  end
 end
 
 command :Ninez do |msg|
     msg.respond "```“Goku”```"
-  end
 end
 
 command :Haku do |msg|
     msg.respond "```“Hehe boi //Proceed to use SWAX//”```"
-  end
 end
 
 command :Tea do |msg|
     msg.respond "https://static.clubeextra.com.br/img/uploads/1/954/12307954.jpeg"
-  end
 end
 command :Lian do |msg|
     msg.respond "```Someone called me?```"
     msg.respond "https://cdn.discordapp.com/attachments/919517210140114987/928811977302740993/deviljho-fire-breathing.gif"
-  end
 end
 end
