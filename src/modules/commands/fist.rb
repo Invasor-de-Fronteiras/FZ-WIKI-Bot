b963a0da0572fcd30f4d8100e1dea0d1b0c81310
Hantzu <pvnobres@gmail.com>
2022-01-06 22:24:29 -0300

    0.0.0.20

--- src/modules/commands/fist.rb
+++
@@ -1,9 +0,0 @@
-module Bot::DiscordCommands
-    module Monsters
-      extend Discordrb::Commands::CommandContainer
-      command :Fist do |msg|
-        msg.respond "```ALL THE FUCKING FILES ARE STOLEN```"
-      
-      end
-    end
-  end
--- src/modules/commands/hantzu.rb
+++ src/modules/commands/hantzu.rb
@@ -5,7 +5,7 @@ module Bot::DiscordCommands
       command :Hantzu do |msg|
 
       msg.respond "Can i feed my Halk with my tears?
-                                  v 0.0.0.18"
+                                  v 0.0.0.20"
 
     end
   end
