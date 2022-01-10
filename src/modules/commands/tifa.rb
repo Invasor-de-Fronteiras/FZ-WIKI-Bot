f4805ac124940f95ad912f609318d7ad4247247f
Hantzu <pvnobres@gmail.com>
2022-01-10 20:15:48 -0300

    0.0.0.22

---
+++ src/modules/commands/tifa.rb
@@ -0,0 +1,11 @@
+module Bot::DiscordCommands
+    module Monsters
+      extend Discordrb::Commands::CommandContainer
+
+      command :Tifa do |msg|
+
+      msg.respond "https://media.tenor.co/videos/7349b0ed35fa6536ca9b040b11cb738d/mp4"
+
+    end
+  end
+end
---
+++ src/modules/commands/tyrannys.rb
@@ -0,0 +1,12 @@
+module Bot::DiscordCommands
+    module Monsters
+      extend Discordrb::Commands::CommandContainer
+
+      command :Tyrannys do |msg|
+
+      msg.respond "```„CHECK THE GODAMN PIN“
+      „Pro weebspike User sub to my Channel for epic content“```"
+
+    end
+  end
+end
