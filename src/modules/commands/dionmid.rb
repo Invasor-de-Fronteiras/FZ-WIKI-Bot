ef65f8fd55eb51581522546c1a178db171a2482d
Hantzu <pvnobres@gmail.com>
2022-02-07 18:00:41 -0300

    0.0.0.24

--- src/modules/commands/dionmid.rb
+++ src/modules/commands/dionmid.rb
@@ -1,7 +1,7 @@
 module Bot::DiscordCommands
     module Monsters
       extend Discordrb::Commands::CommandContainer
-      command :Dionmid do |msg|
+      command :Ultra_Dionmid do |msg|
         msg.respond "```“per aspera ad astra…
     …ok, sapete perché i pesci hanno le spine? Perché in acqua c’è la corrente!”```"
         msg.respond "https://media.discordapp.net/attachments/919517210140114987/928811307786989568/IMG_0131.jpg"
---
+++ src/modules/commands/void.rb
@@ -0,0 +1,10 @@
+module Bot::DiscordCommands
+    module Monsters
+      extend Discordrb::Commands::CommandContainer
+      command :Rhobiusvoid: do |msg|
+        msg.respond "```to another 12 years of frontier, but read the pastebin first 
+        - one-eyed fuck```"
+      
+      end
+    end
+  end
