1ce3dc041461dfeae1c9fef90f28e423c2227896
Hantzu <pvnobres@gmail.com>
2022-02-15 12:13:41 -0300

    0.0.0.272

--- src/modules/commands/a_lot_of_miscelanous.rb
+++ src/modules/commands/a_lot_of_miscelanous.rb
@@ -2,7 +2,7 @@ module Bot::DiscordCommands
     module Monsters
       extend Discordrb::Commands::CommandContainer
       bot.message do |event|
-        event.respond "Type mhf!Help to see how the bot works #{event.user.name}"
+        event.respond "Type mhf!Help to see how the bot works"
       
       end
     end
