9b2dd5cdf98b92cc37218533fd4274238a4a7069
Hantzu <pvnobres@gmail.com>
2022-03-13 01:08:02 -0300

    0.0.0.33

--- src/modules/events/react.rb
+++ src/modules/events/react.rb
@@ -3,12 +3,16 @@ module Bot::DiscordEvents
     
         extend Discordrb::EventContainer
         
-        message(contains: /cheat shop/i, bucket: :delay10) do |event|
-          event.message.respond "Stop cheating you dumb, just play the game!"
+        message(contains: /cheat shop/i, bucket: :delay10000) do |event|
+          event.message.respond "Stop cheating bro, it can easily ruin your experience, use at your own risk!"
         end
         
-        message(contains: /(^|\s)gay($|\s)/i) do |event|
+        message(contains: /(^|\s)gay($|\s)/i, bucket: :delay10000) do |event|
           event.message.react "🏳️‍🌈"
+        end
+
+        message(contains: /(^|\s)hantzu($|\s)/i, bucket: :delay10000) do |event|
+          event.message.react "🧙‍♂️"
         end
         
       end
--- src/modules/events/ready.rb
+++ src/modules/events/ready.rb
@@ -4,6 +4,7 @@ module Bot::DiscordEvents
     ready do |event|
       event.bot.game = Bot::CONFIG.game
       puts "MHFZ WIKI is ready with #{event.bot.servers.count} servers and #{event.bot.users.count} users!"
+      puts "Logged in as #{event.bot.profile.username} | #{event.bot.servers.size} servers"
     end
   end
 end
