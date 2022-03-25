e91a738c7e8678c35d018965c3e4c934bbb33b68
Hantzu <pvnobres@gmail.com>
2022-03-25 14:19:05 -0300

    0.0.0.48

--- src/modules/events/react.rb
+++ src/modules/events/react.rb
@@ -18,33 +18,49 @@ module Bot::DiscordEvents
         end
 
         message(contains: /(^|\s)hantzu($|\s)/i, bucket: :delay1000) do |event|
-          event.message.respond "Did you mean Gatekeeptzu?"
+          event.message.respond "He's not a problem anymore <:maiGun:919705122135744592>"
           puts ">
 
-          ╔══════════════════════════════════════════════════════════════════════╗
-          ║ MHFZ WIKI was summoned to: #{event.server.name} (#{event.server.id}) ║
-          ╠══════════════════════════════════════════════════════════════════════╣
-          ║ by #{event.author.username}                                          ║
-          ║ in the reign of the King #{event.server.owner.name}                  ║
-          ╚══════════════════════════════════════════════════════════════════════╝
+╔══════════════════════════════════════════════════════════════════════╗
+║ MHFZ WIKI was summoned to: #{event.server.name} (#{event.server.id}) ║
+╠══════════════════════════════════════════════════════════════════════╣
+║ by #{event.author.username}                                          ║
+║ in the reign of the King #{event.server.owner.name}                  ║
+╚══════════════════════════════════════════════════════════════════════╝
           
-          <"
+<"
         end
         
         message(contains: /(^|\s)corrompeu($|\s)/i, bucket: :delay1000) do |event|
           event.message.respond "#{event.user.name} bem vindo a tropa dos corrompedores de save 🤡🤡🤡"
         end
 
-        message(contains: /(^|\s)onde pega($|\s)/i, bucket: :delay1000) do |event|
-          event.message.respond "https://media.giphy.com/media/J5q4qtKqQ4plPl4YJN/giphy.gif"
-        end
-
         message(contains: /(^|\s)mai($|\s)/i, bucket: :delay1000) do |event|
           event.message.react "⭐"
+          puts ">
+
+╔══════════════════════════════════════════════════════════════════════╗
+║ MHFZ WIKI was summoned to: #{event.server.name} (#{event.server.id}) ║
+╠══════════════════════════════════════════════════════════════════════╣
+║ by #{event.author.username}                                          ║
+║ in the reign of the King #{event.server.owner.name}                  ║
+╚══════════════════════════════════════════════════════════════════════╝
+
+<"
         end
 
         message(contains: /(^|\s)luna($|\s)/i, bucket: :delay1000) do |event|
           event.message.react "🌙"
+          puts ">
+
+╔══════════════════════════════════════════════════════════════════════╗
+║ MHFZ WIKI was summoned to: #{event.server.name} (#{event.server.id}) ║
+╠══════════════════════════════════════════════════════════════════════╣
+║ by #{event.author.username}                                          ║
+║ in the reign of the King #{event.server.owner.name}                  ║
+╚══════════════════════════════════════════════════════════════════════╝
+
+<"
         end
 
         message(contains: /(^|\s)Malck($|\s)/i, bucket: :delay1000) do |event|
