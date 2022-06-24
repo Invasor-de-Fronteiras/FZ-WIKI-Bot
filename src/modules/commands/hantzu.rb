242b5e566668c2a31a2506280cf83bfe6b2d74f5
Hantzu <pvnobres@gmail.com>
2022-06-24 16:08:18 -0300

    0.0.3.1d

--- src/modules/commands/hantzu.rb
+++ src/modules/commands/hantzu.rb
@@ -8,7 +8,7 @@ module Bot::DiscordCommands
           msg.respond "🤡"
       else
       msg.send "Can i feed my Halk with my tears?
-                            v 0.0.3.1c"
+                            v 0.0.3.1d"
 
       end
     end
