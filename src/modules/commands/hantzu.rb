6b8e1a8b3bfb1fee2105e8eb2ea46858e8462b70
Hantzu <pvnobres@gmail.com>
2022-08-08 10:28:52 -0300

    0.0.3.9i

--- src/modules/commands/hantzu.rb
+++ src/modules/commands/hantzu.rb
@@ -6,7 +6,7 @@ module Bot::DiscordCommands
 
       event.channel.send_embed do |embed|
         embed.description = "Can i feed my Halk with my tears?
-                                        v 0.0.3.9h"
+                                        v 0.0.3.9i"
         embed.colour = 0xff8040
         embed.author = Discordrb::Webhooks::EmbedAuthor.new(
             name: event.author.display_name,
