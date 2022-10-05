e8550df4103fe7eee2f0108d2d7dec8cbfba6c1d
Yslan Ramos <yslanramos.r@gmail.com>
2022-10-05 01:08:25 -0300

    feat: move emojis to private server

--- src/lib/emojis.ts
+++ src/lib/emojis.ts
@@ -1,40 +1,30 @@
+const emojis: Record<string, string> = {
+  blast: "<:blast:1027061466920140810>",
+  bleeding: "<:bleed:1027062492976926730>",
+  blind: "<:blind:1027062540406104176>",
+  confusion: "<:confusion:1027062573792768020>",
+  crystallization: "<:crystal:1027062600418218044>",
+  defensedown: "<:defdown:1027062627840577617>",
+  corruptedpoison: "<:durepoison:1027062658861649990>",
+  soiled: "<:fart:1027062683385741352>",
+  frenzyvirus: "<:frenzy:1027062698556542976>",
+  frostbite: "<:frost:1027062718215237683>",
+  stun: "<:stun:1027062749768978574>",
+  dragon: "<:zdragon:1027062851615084584>",
+  zerosion: "<:zerosion:1027062868379717703>",
+  fire: "<:zfire:1027062883529531493>",
+  ice: "<:zice:1027062903087579227>",
+  magnet: "<:zmagnet:1027062922972758026>",
+  paralysis: "<:zpara:1027062941931012126>",
+  poison: "<:zpoison:1027062957659664415>",
+  sleep: "<:zsleep:1027062976936677437>",
+  snowman: "<:zsnowman:1027062994816991283>",
+  thunder: "<:zthunder:1027063010734395433>",
+  water: "<:zwater:1027063037275938816>",
+};
+
 export const getEmoji = (name: string): string => {
-  const input = name?.replace(/Extreme|\s|blight/gi, "");
+  const input = name?.replace(/Extreme|\s|blight/gi, "").toLowerCase();
 
-  switch (input) {
-    case "zerosion":
-      return `${name} <:zerosion:883123230754349066>`;
-    case "Fire":
-      return `${name} <:zfire:883120156916744312>`;
-    case "zmagnet":
-      return `${name} <:zmagnet:883123524229820487>`;
-    case "Paralysis":
-      return `${name} <:zpara:883122888692076545>`;
-    case "Poison":
-      return `${name} <:zpoison:883122917976719451>`;
-    case "CorruptedPoison":
-      return `${name} <:durepoison:883123342914236446>`;
-    case "Sleep":
-      return `${name} <:zsleep:883122848888143892>`;
-    case "Ice":
-      return `${name} <:zsnowman:883124811746926592>`;
-    case "Thunder":
-      return `${name} <:zthunder:883120020312440852>`;
-    case "Water":
-      return `${name} <:zwater:883120128017965066>`;
-    case "DefenseDown":
-      return `${name} <:defdown:883125247551893514>`;
-    case "Stun":
-      return `${name} <:stun:883125464397398106>`;
-    case "Dragon":
-      return `${name} <:zdragon:883119756528476161>`;
-    case "FrenzyVirus":
-      return `${name} <:frenzy:883124230357680199>`;
-    case "Magnet":
-      return `${name} <:zmagnet:883123524229820487>`;
-    case `DracophageErosion`:
-      return `${name} <:zerosion:883123230754349066>`;
-  }
-
-  return name;
+  return `${name} ${emojis[input] ? emojis[input] : ""}`;
 };
